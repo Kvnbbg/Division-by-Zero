e8052cd1960a0771ab51f8f43881850c72bfacdd
Kevin Marville <116537485+Kvnbbg@users.noreply.github.com>
2026-09-03 11:07:43 +0200

    Add Spring Boot application entrypoint for TDAAH agent.

---
+++ agent-spring-boot/src/main/java/fr/kvnbbg/tdaah/TdaahAgentApplication.java
@@ -0,0 +1,12 @@
+package fr.kvnbbg.tdaah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdaahAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdaahAgentApplication.class, args);
+    }
+}
